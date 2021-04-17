5e10fecca9c02ee32a99a7242e4ff2996ca110fa
Basran, Paramdev <PB56297@worksafebc.com>
2021-04-17 11:23:06 -0700

    Section 1

--- cypress/integration/section-1.spec.js
+++ cypress/integration/section-1.spec.js
@@ -1,10 +1,87 @@
+
 const { Section1 } = require('../objects/section-1')
 
-describe('Problem 1', () => {
-  /**
-   * Example:
-   * To access assertSampleApiResponse() from Section1, you can do: Section1.actions.assertSampleApiResponse();
-   *
-   * Test away!
-   */
+describe('Section 1', () =>
+{
+    beforeEach(async()=>
+    {
+        await cy.visit('section-1');
+    })
+
+
+    it('Show Table Button is displayed', async() => {
+        await Section1.actions.assertTableButtonIsPresentOrNot();
+    })
+
+    it('Assert Table is not present by default', async() => {
+        await Section1.actions.assertTableIsPresentOrNot();
+    })
+
+    it('Assert Table has 5 Columns', async() => {
+        await Section1.actions.clickOnShowTableButton();
+        await Section1.actions.assertTableHasExpectedNumberOfColumns();
+    })
+
+    it('Assert Table has 10 rows', async() => {
+        await Section1.actions.clickOnShowTableButton();
+        await Section1.actions.assertTableHasExpectedNumberOfRows();
+    })
+
+    it('Validate that Admin has ID as 1', async() => {
+        await Section1.actions.clickOnShowTableButton();
+        await Section1.actions.assertIDAdminHasIDas1();
+    })
+
+
+    it('Validate that role user is assigned to greater than 5 user', async() => {
+        await Section1.actions.clickOnShowTableButton();
+        await Section1.actions.assertRoleUserIsAssignedToMoreThan5Users();
+    })
+
+    it('Validate exactly 3 people are older than 60', async() => {
+        await Section1.actions.clickOnShowTableButton();
+        await Section1.actions.assertExactly3PeopleAreOlderThan60();
+     })
+
+    it('Assert form is not visible', async () => {
+        await Section1.actions.assertFormIsNotVisibleByDefault();
+    })
+
+    it('Validate form is visible after clicking on Show form button', async () => {
+        await Section1.actions.clickOnShowFormButton();
+        await Section1.actions.assertFormIsVisibleAfterClickingOnShowForm();
+    })
+
+    it('On Form section Fill in the Name and Age inputs, and assert that both inputs are filled', async() => {
+        await Section1.actions.clickOnShowFormButton();
+        await Section1.actions.enterNameInFormAndValidateItsPoplated('testing name');
+        await Section1.actions.enterAgeInFormAndValidateItsPoplated('35');
+    })
+
+    it('On Form section Select Female from the select option, and assert that the value is female', async() => {
+        await Section1.actions.clickOnShowFormButton();
+        await Section1.actions.selectGenderInFormAndValidateItsValueIsSelectedProperly('female');
+    })
+
+    it('On Form section Tick the Nurse checkbox and assert that the value nurse is true', async() => {
+        await Section1.actions.clickOnShowFormButton();
+        await Section1.actions.clickOnCheckBoxOfNurseAndValidateItsValue();
+    })
+
+    it('On Form section Click on the Submit button and assert that there is an alert window showing with the text Form submitted!', async() => {
+        await Section1.actions.clickOnShowFormButton();
+        await Section1.actions.enterNameInFormAndValidateItsPoplated('testing name');
+        await Section1.actions.enterAgeInFormAndValidateItsPoplated('35');
+        await Section1.actions.clickOnCheckBoxOfNurseAndValidateItsValue('female');
+        await Section1.actions.clickOnCheckBoxOfNurseAndValidateItsValue();
+        await Section1.actions.clickOnSubmitButtonOnFormAndValidateThePopUp();
+    })
+
+
+
+    afterEach(async() =>
+    {
+        cy.screenshot();
+    }
+)
 })
--- cypress/objects/section-1.js
+++ cypress/objects/section-1.js
@@ -1,40 +1,128 @@
 const Section1 = {
-  /**
-   * A literal is considered static, stable strings (eg. titles, form labels, ...)
-   */
-  literals: {
-    SAMPLE_LITERAL: 'This is a sample literal. You can safely delete it.',
-  },
-
-  /**
-   * An element is a selector for any DOM element (eg. [data-test="xxx"], #id, ...)
-   */
-  elements: {
-    sampleElement: '[data-test=sample-element-to-be-safely-deleted]',
-  },
-
-  /**
-   * An action should be pretty self explanatory! It consists of all the method performing
-   * a particular action from clicking a simple button to doing complex assertions.
-   */
-  actions: {
     /**
-     * Example of action.
-     * In this example, we are grabbing a sample element, clicking on it and asserting the api answer.
-     *
-     * This is only used as an example and can be safely deleted.
+     * A literal is considered static, stable strings (eg. titles, form labels, ...)
      */
-    assertSampleApiResponse () {
-      cy.server()
-      cy.wait('/endpoint').as('endpoint')
-
-      cy.get(Section1.elements.sampleElement).click()
-      // ... An api call to "/endpoint" performed on the app.
-      cy.wait('@endpoint').should((request) => {
-        expect(request.status).to.eq(200)
-      })
+    literals: {
+        SAMPLE_LITERAL: 'This is a sample literal. You can safely delete it.',
     },
-  },
+
+    /**
+     * An element is a selector for any DOM element (eg. [data-test="xxx"], #id, ...)
+     */
+    elements: {        
+        showTableButton: "#table-toggle-button",
+        tableID: "#alaya-table",
+        tableHeader: ".table-header",
+        countColumns: "#alaya-table>tbody>tr>th",
+        countRows: "#alaya-table>tbody>tr",
+        roleColumn: "#alaya-table>tbody>tr th:nth-child(4)",
+        DOBColumn: "#alaya-table>tbody>tr th:nth-child(3)",
+        formID: "#alaya-form",
+        showFormButton: "#form-toggle-button",
+        formNameInput: "#fullName",
+        formAgeInput: "#age",
+        formGenderDropDown: "#gender",
+        formNurseCHeckBox: "#nurse",
+        formSubmitButton:"#submit"
+    },
+
+    /**
+     * An action should be pretty self explanatory! It consists of all the method performing
+     * a particular action from clicking a simple button to doing complex assertions.
+     */
+    actions: {
+        /**
+         * Example of action.
+         * In this example, we are grabbing a sample element, clicking on it and asserting the api answer.
+         *
+         * This is only used as an example and can be safely deleted.
+         */
+        async clickOnShowTableButton() {
+            await cy.get(Section1.elements.showTableButton).click()
+        },
+
+        async clickOnShowFormButton() {
+            await cy.get(Section1.elements.showFormButton).click()
+        },
+
+        async enterNameInFormAndValidateItsPoplated(name: String) {
+            await cy.get(Section1.elements.formNameInput).type(name).should('have.value', name)
+        },
+
+        async enterAgeInFormAndValidateItsPoplated(age: String) {
+            await cy.get(Section1.elements.formNameInput).type(age).should('have.value', age)
+        },
+
+        async selectGenderInFormAndValidateItsValueIsSelectedProperly(gender: String) {
+            await cy.get('select').select(gender)
+            await cy.get('select#gender option:selected').should('have.text',gender)
+        },      
+
+        async clickOnCheckBoxOfNurseAndValidateItsValue() {
+            await cy.get(Section1.elements.formNurseCHeckBox).check({
+                force: true
+            }).should('be.checked')
+        },
+
+        async clickOnSubmitButtonOnFormAndValidateThePopUp() {
+            await cy.get(Section1.elements.formSubmitButton).click()
+            await cy.on('window:alert', 'Form submitted!') 
+        },
+
+        async assertTableButtonIsPresentOrNot() {
+            await cy.get(Section1.elements.showTableButton).should('be.visible')
+        },
+
+        async assertTableIsPresentOrNot() {
+            await cy.get(Section1.elements.tableID).should('not.be.visible')
+        }, 
+        async assertTableHasExpectedNumberOfColumns() {
+            await cy.get(Section1.elements.countColumns).should('have.length', 5)
+        },
+
+        async assertTableHasExpectedNumberOfRows() {
+            await cy.get(Section1.elements.countRows).should('have.length', 10)
+        },
+
+        async assertIDAdminHasIDas1() {
+            await cy.get(Section1.elements.countRows).contains('admin').parent().within(function ()
+            {
+                cy.get('th').eq(0).should('contain.text', '1')
+            }
+            )
+        },
+
+        async assertRoleUserIsAssignedToMoreThan5Users()
+        {
+            var count = 0;
+            await cy.get(Section1.elements.roleColumn).each(($e,index,$list) => {
+                const text = $e.text()
+                if (text.includes("user")) {
+                    count++
+                }
+            })
+            expect(count).to.be.greaterThan(5)
+        },
+
+        async assertExactly3PeopleAreOlderThan60() {
+            var count = 0;
+            await cy.get(Section1.elements.roleColumn).each(($e, index, $list) => {
+                const dob = $e.text().to
+                if (text.includes("user")) {
+                    count++
+                }
+            })
+            expect(count).to.be.greaterThan(5)
+        },
+
+        async assertFormIsNotVisibleByDefault() {
+            await cy.get(Section1.elements.formID).should('not.be.visible')
+        },
+
+        async assertFormIsVisibleAfterClickingOnShowForm() {
+            await cy.get(Section1.elements.formID).should('be.visible')
+        },
+    },
 }
 
 module.exports = { Section1 }
