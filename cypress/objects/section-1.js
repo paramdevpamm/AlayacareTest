8af9237378c071a101adaead282cd51fadd370c0
Basran, Paramdev <PB56297@worksafebc.com>
2021-04-18 15:31:35 -0700

    Clean up test comments

--- cypress/objects/section-1.js
+++ cypress/objects/section-1.js
@@ -1,14 +1,4 @@
 const Section1 = {
-    /**
-     * A literal is considered static, stable strings (eg. titles, form labels, ...)
-     */
-    literals: {
-        SAMPLE_LITERAL: 'This is a sample literal. You can safely delete it.',
-    },
-
-    /**
-     * An element is a selector for any DOM element (eg. [data-test="xxx"], #id, ...)
-     */
     elements: {        
         showTableButton: "#table-toggle-button",
         tableID: "#alaya-table",
@@ -27,17 +17,8 @@ const Section1 = {
         formSubmitButton:"#submit"
     },
 
-    /**
-     * An action should be pretty self explanatory! It consists of all the method performing
-     * a particular action from clicking a simple button to doing complex assertions.
-     */
     actions: {
-        /**
-         * Example of action.
-         * In this example, we are grabbing a sample element, clicking on it and asserting the api answer.
-         *
-         * This is only used as an example and can be safely deleted.
-         */
+       
         async clickOnShowTableButton() {
                 await cy.get(Section1.elements.showTableButton).click()
         },
